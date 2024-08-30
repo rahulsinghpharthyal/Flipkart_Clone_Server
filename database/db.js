5e4a9f6a3bd823f24f004aecdefc692ba1172bb0
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2024-08-30 11:31:22 +0530

    server deployed

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 import mongoose  from "mongoose";
 
 export const Connection = async () =>{
-    const URL = 'mongodb://localhost:27017';
+    // const URL = 'mongodb://localhost:27017';
+    const URL = process.env.MONGO_URL;
     try{
-      await mongoose.connect(URL, { useNewUrlParser: true })
-      console.log('Database Connect Succesfully')
+      const data = await mongoose.connect(URL)
+      console.log(`Database Connect Succesfully, ${data.connection.host}`)
     }catch(error){
         console.log('Error while connecting with the database', error.message)
     }
